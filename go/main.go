a106413ed74c5babf8ad8a4fd052fbed190f238f
Łukasz Włodarczyk <cyberwlodarczyk@proton.me>
2024-10-27 13:34:19 +0100

    [go] manage user data in cookies

--- go/main.go
+++ go/main.go
@@ -1,106 +1,67 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
+	"net/url"
 	"os"
 	"os/exec"
 	"strings"
-	"sync"
 )
 
-type User struct {
-	Name string
+type Session struct {
+	Username string `json:"username"`
+	Template string `json:"template"`
 }
 
-func (u User) Log() string {
-	cmd := exec.Command("bash", "-c", fmt.Sprintf("echo %s", u.Name))
+func (s Session) Log() string {
+	cmd := exec.Command("bash", "-c", fmt.Sprintf("echo %s", s.Username))
 	out, _ := cmd.CombinedOutput()
 	return string(out)
 }
 
-type session struct {
-	user     User
+type handler struct {
 	template string
 }
 
-type server struct {
-	template string
-	sessions map[string]session
-	mutex    sync.Mutex
-}
-
-func createKey() string {
-	b := make([]byte, 16)
-	if _, err := rand.Read(b); err != nil {
-		panic(err)
-	}
-	return hex.EncodeToString(b)
-}
-
-func createCookie(k string) *http.Cookie {
-	return &http.Cookie{
-		Name:     "session",
-		Value:    k,
-		Path:     "/",
-		HttpOnly: true,
-		MaxAge:   60 * 60,
-	}
-}
-
-func (s *server) createSession() (session, string) {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
-	k := createKey()
-	ss := session{template: "CHANGE ME"}
-	s.sessions[k] = ss
-	return ss, k
-}
-
-func (s *server) getSession(k string) (session, bool) {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
-	ss, ok := s.sessions[k]
-	return ss, ok
-}
-
-func (s *server) editSession(k string, ss session) bool {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
-	if _, ok := s.sessions[k]; !ok {
-		return false
-	}
-	s.sessions[k] = ss
-	return true
-}
-
-func (s *server) handler(w http.ResponseWriter, r *http.Request) {
-	var ss session
-	var ok bool
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var s Session
 	c, err := r.Cookie("session")
 	if err != nil {
-		ok = false
+		s = Session{"", "CHANGE ME"}
 	} else {
-		ss, ok = s.getSession(c.Value)
-	}
-	if !ok {
-		var k string
-		ss, k = s.createSession()
-		c = createCookie(k)
-		http.SetCookie(w, c)
+		v, err := url.QueryUnescape(c.Value)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if err = json.Unmarshal([]byte(v), &s); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
 	}
 	if r.Method == http.MethodPost {
 		if err := r.ParseForm(); err != nil {
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
-		s.editSession(
-			c.Value,
-			session{User{r.FormValue("name")}, r.FormValue("template")},
+		v, err := json.Marshal(
+			Session{r.FormValue("username"), r.FormValue("template")},
 		)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		http.SetCookie(w, &http.Cookie{
+			Name:     "session",
+			Value:    url.QueryEscape(string(v)),
+			Path:     "/",
+			SameSite: http.SameSiteStrictMode,
+			HttpOnly: true,
+			MaxAge:   60 * 60,
+		})
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	} else if r.Method != http.MethodGet {
@@ -108,22 +69,23 @@ func (s *server) handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	t, err := template.New("index.html").Parse(
-		strings.Replace(s.template, "CHANGE ME", ss.template, 1),
+		strings.Replace(h.template, "CHANGE ME", s.Template, 1),
 	)
 	if err != nil {
 		panic(err)
 	}
 	w.Header().Add("Content-Type", "text/html; charset=utf-8")
-	if err = t.Execute(w, ss.user); err != nil {
+	if err = t.Execute(w, s); err != nil {
 		panic(err)
 	}
 }
 
 func main() {
-	t, err := os.ReadFile("templates/index.html")
+	t, err := os.ReadFile("index.html")
 	if err != nil {
 		panic(err)
 	}
-	s := &server{template: string(t), sessions: make(map[string]session)}
-	http.ListenAndServe(":4444", http.HandlerFunc(s.handler))
+	if err = http.ListenAndServe(":4444", &handler{string(t)}); err != nil {
+		panic(err)
+	}
 }
