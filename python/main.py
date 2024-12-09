9c91d70e53600438c21cf055ae408ebabba9462f
JakubZeslawski <147949808+JakubZeslawski@users.noreply.github.com>
2024-12-09 14:47:35 +0100

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,13 +1,16 @@
-from flask import Flask, request, render_template_string, render_template
+from flask import Flask, render_template_string, request, render_template
+import os
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = os.urandom(24)
 
+def filter(user_input: str) -> str:
+    return user_input.strip().replace('_', '').replace('.', '')
 @app.route("/", methods=["GET", "POST"])
+
 def index():
-    if request.method == "POST":
-		username = request.form.get("username")
+	if request.method == "POST":
+		username = fliter(request.form.get("username"))
 		comment = request.form.get("comment")
 		if not username or not comment:
             		return "Brak nazwy uzytkownika lub komentarza"
@@ -15,6 +18,5 @@ def index():
 		return render_template("index.html", output=output)
 	
 	return render_template("index.html")
-
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
