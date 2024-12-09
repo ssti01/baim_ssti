550e5bb0df9050bd7e1bab1383236d4aff11f0cd
Bogdan <92211934+maybe91@users.noreply.github.com>
2024-12-09 22:27:59 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,16 +2,27 @@ import fs from "fs";
 import express from "express";
 import handlebars from "handlebars";
 
-const html = fs.readFileSync("index.html", "utf-8");
+const hbs = fs.readFileSync("index.hbs", "utf-8");
 const app = express();
+
+
+process.env.FLAG = 'test';
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
-  res.send(
-    req.query.template
-      ? handlebars.compile(html.replace("CHANGE ME", req.query.template))()
-      : html
-  );
+
+  const UserInput = req.query.template || "Domyślny opis";
+  const html = hbs.replace("{{ description }}", UserInput);
+
+
+  const template = handlebars.compile(html);
+  const result = template({});
+
+  res.send(result);
 });
-app.listen(4444);
+
+app.listen(4444, () => {
+  console.log("Serwer działa na porcie 4444");
+});
